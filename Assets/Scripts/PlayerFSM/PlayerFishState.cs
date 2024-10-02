56e788c6a2c5edcd160f739d3210bedffec2cfcf
unknown <joyx@students.cs.ubc.ca>
2024-10-01 22:35:19 -0700

    added catch animation - WIP code in PlayerFishState to test animation transitions

--- Assets/Scripts/PlayerFSM/PlayerFishState.cs
+++ Assets/Scripts/PlayerFSM/PlayerFishState.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerFishState : PlayerState
 {
+    // just setting this bool to true for now to test the animation
+    bool fishHooked = true;
     public PlayerFishState(Player player, PlayerStateMachine stateMachine) : base(player, stateMachine)
     {
     }
@@ -25,10 +27,30 @@ public class PlayerFishState : PlayerState
 
     public override void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.F))
+        // finish showing off the fish that was just caught (the FishCaught boolean is still true so its still in the caught state)
+        // Debug.Log(player.Animator.GetBool("FishCaught"));
+        if (player.Animator.GetBool("FishCaught") && Time.time > (this.startTime + 1.5f))
         {
             stateMachine.ChangeState(player.BoatState);
         }
+        // change to fishcatch sprite, starts the timer so that the catch sprite stays for 1 second
+        // TODO: here all the fish data should be added to inventory, display fish sprite and basic info
+        // TODO: the fishHooked condition should be linked to some kinf of fish data, and indicates that the player has a fish on the line
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            player.Animator.SetBool("IsFishing", false);
+            if (fishHooked)
+            {
+                Debug.Log("fish caught");
+                player.Animator.SetBool("FishCaught", true);
+                this.startTime = Time.time;
+                
+            } else
+            {
+                stateMachine.ChangeState(player.BoatState);
+            }
+            
+        }
+
     }
 }
