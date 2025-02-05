19d2978185d8d16970dac870d795739c5855eb14
EllynChan <eve9323@163.com>
2025-02-04 23:19:54 -0800

    intro dialogue test text functioning

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -12,7 +12,7 @@ public class DialogueManager : MonoBehaviour
         public Dictionary<string, ConversationLine> conversation;
         public PopUp popup;
         public int num_lines;
-        public int? texture_ally;
+        public int? texture_ally; // ally1=51, ally2=80
     }
 
     [System.Serializable]
@@ -39,8 +39,13 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private Image allyImage;
 
     [SerializeField] private TextAsset dialogueJson; // Reference to JSON
+    [SerializeField] private List<Sprite> mcSprites;
+    [SerializeField] private List<Sprite> ally1Sprites;
+    [SerializeField] private List<Sprite> ally2Sprites;
+
     private Dictionary<string, DialogueEvent> dialogues;
     private Queue<ConversationLine> currentDialogueQueue;
+    private DialogueEvent currentDialogueEvent;
 
     void Awake()
     {
@@ -53,26 +58,63 @@ public class DialogueManager : MonoBehaviour
             Destroy(gameObject);
         }
         dialogues = JsonConvert.DeserializeObject<Dictionary<string, DialogueEvent>>(dialogueJson.text);
-
-        /*foreach (string k in dialogues.Keys)
-        {
-            Debug.Log(k);
-        }*/
+        currentDialogueQueue = new Queue<ConversationLine>(); 
     }
 
-    void StartDialogue(string eventKey)
+    public void StartDialogue(string eventKey)
     {
         uiCanvas.SetActive(true);
+        //Debug.Log(dialogues[eventKey].conversation["line_0"].text);
+        currentDialogueEvent = dialogues[eventKey];
+        for (int i = 0; i < currentDialogueEvent.num_lines; i++)
+        {
+            currentDialogueQueue.Enqueue(currentDialogueEvent.conversation["line_"+i]);
+        }
+        NextDialogue();
+    }
+
+    public void NextDialogue()
+    {
+        if (currentDialogueQueue.Count < 1)
+        {
+            EndDialogue();
+            return;
+        }
+
+        ConversationLine cl = currentDialogueQueue.Dequeue();
+        dialogueText.text = cl.text;
+        nameText.text = cl.name;
+
+        /*if (cl.frame_mc != null)
+        {
+            mcImage.sprite = mcSprites[ (int) cl.frame_mc];
+        }
+        else
+        {
+            mcImage = null;
+        }
+
+        if (cl.frame_ally != null)
+        {
+            if (currentDialogueEvent.texture_ally == 51)
+            {
+                allyImage.sprite = ally1Sprites[(int)cl.frame_ally];
+            } 
+            else if (currentDialogueEvent.texture_ally == 80)
+            {
+                allyImage.sprite = ally2Sprites[(int)cl.frame_ally];
+            }
+        } 
+        else
+        {
+            allyImage = null;
+        }*/
+
     }
 
     void EndDialogue()
     {
         uiCanvas.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // handle popup
     }
 }
